914ed64257b178b19490bc8da85218b08a52d046
WDEXcourse <86914835+WDEXcourse@users.noreply.github.com>
2021-07-11 11:34:52 +0900

    test

---
+++ Assets/DoorMove.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorMove : MonoBehaviour
+{
+    public float speed;
+
+    public float radius;
+
+    float angle = 0;
+
+    Vector3 pos;
+    Vector3 start;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pos = this.transform.position;
+        start = pos;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        pos.x = radius * Mathf.Sin(angle);
+        pos.x += start.x;
+        angle += speed;
+
+        this.transform.position = pos;
+    }
+}
