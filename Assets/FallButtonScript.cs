52bce8f709cc13e3190ca672891e1b16ca00c52e
WDEXcourse <wdkawasaki.online@gmail.com>
2021-08-01 11:33:23 +0900

    落ち葉の複数落ちができた。

---
+++ Assets/FallButtonScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallButtonScript : MonoBehaviour
+{
+public GameObject[] leaves;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0;i < leaves.Length; i++)
+        {
+            leaves[i].SetActive(false);
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            for (int i = 0; i < leaves.Length; i++)
+            {
+                leaves[i].SetActive(true);
+            }
+        }
+    }
+
+
+}
+
