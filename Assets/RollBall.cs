be87ec4c81d6ce5f32a780be4f0510ce827c1454
WDEXcourse <wdkawasaki.online@gmail.com>
2021-08-22 11:32:11 +0900

    ゴミが転がるようになった

---
+++ Assets/RollBall.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RollBall : MonoBehaviour
+{
+    public Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb.AddForce(-100, 0, 0, ForceMode.Impulse);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
