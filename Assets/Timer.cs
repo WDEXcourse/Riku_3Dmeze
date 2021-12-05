fca60afbc53e28d9fed4468871f16d50e5d73257
WDEXcourse <wdkawasaki.online@gmail.com>
2021-12-05 11:27:53 +0900

    円形タイマー完成

--- Assets/Timer.cs
+++ Assets/Timer.cs
@@ -6,22 +6,36 @@ using UnityEngine.UI;
 public class Timer : MonoBehaviour
 {
     float countTime = 300;
+    public Image timeimeage;
 
 
     // Use this for initialization
     void Start()
     {
-        
+        timeimeage.color = Color.green;
     }
 
     // Update is called once per frame
     void Update()
     {
 
+
         countTime -= Time.deltaTime;
 
 
-        GetComponent<Text>().text = countTime.ToString("F2");
-        
+        GetComponent<Text>().text = countTime.ToString("F0");
+
+        timeimeage.fillAmount = countTime/300;
+
+        if (countTime < 30)
+        {
+            timeimeage.color = Color.red;
+        }
+        else if (countTime < 150)
+        {
+            timeimeage.color = Color.yellow;
+        }
+
+
     }
 }
